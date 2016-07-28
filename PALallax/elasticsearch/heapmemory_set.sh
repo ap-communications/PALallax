f35a644eb43b4beb791ab0ff69b95a1aa4008848
naitwo2 <naito5296@gmail.com>
2016-07-28 20:08:39 +0900

    modified:   PALallax/elasticsearch/heapmemory_set.sh

--- PALallax/elasticsearch/heapmemory_set.sh
+++ PALallax/elasticsearch/heapmemory_set.sh
@@ -4,7 +4,7 @@
 totalmem=`free | awk '/Mem/{print $2;}'`
 halfmem=`expr $totalmem / 2 / 1024`
 halfmem=`expr \( $halfmem / 500 \) \* 500`
-
+halfmem=${halfmem}m
 
 #Set heapmemory
 sed -i -e "s/#ES_HEAP_SIZE=2g/ES_HEAP_SIZE=$halfmem/g" /etc/sysconfig/elasticsearch > /dev/null
