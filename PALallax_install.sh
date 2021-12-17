a29efe298ad916ac79cf8269cac6ce1611b2da0c
bonkotu <bonkotusan@gmail.com>
2021-12-17 08:06:05 +0000

    デフォルトのSpaceを更新更新

--- PALallax_install.sh
+++ PALallax_install.sh
@@ -249,6 +249,12 @@ wait
 
 curl -X POST "http://localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@PALallax/kibana/export.ndjson
 
+curl -H "kbn-xsrf: reporting" -H 'Content-Type: application/json' -X PUT "http://localhost:5601/api/spaces/space/default" -d '{
+"id": "default",
+"name": "Default",
+"disabledFeatures": ["apm","maps","canvas","infrastructure","logs","siem","uptime","monitoring","dev_tools","ml","graph"]
+}'
+
 ## Logrotate Settings
 
 
